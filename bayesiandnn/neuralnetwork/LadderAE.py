b8db1bc3d26e04815cf696ddf8569181f1881d6a
Akash <akash.uee@gmail.com>
2016-04-12 23:52:29 +0200

    ladder network-encoder complete ...

--- bayesiandnn/neuralnetwork/LadderAE.py
+++ bayesiandnn/neuralnetwork/LadderAE.py
@@ -2,16 +2,21 @@
 # this file implements a ladder network ..... 
 import logging
 import numpy as np
-from collection import OrderedDict 
+from collections import OrderedDict 
+from os import sys, path
 import os, cPickle, gzip 
-import math
 import theano
 import theano.tensor as T 
 from theano.tensor.shared_randomstreams import RandomStreams 
 
+sys.path.append( path.dirname( path.dirname( path.abspath(__file__) ) ) )
+from utils import ConfusionMatrix, LearningRate
+from utils.LearningRate import *
+from utils import *
+
 
 class LadderAE(object):
-	def __init__(self, x_labelled=None, x_unlabelled=None, y_labelled=None, hidden_layer_config, corruption_layer_config, activation_layer_config, learning_rate_config=[0.02], random_seed=1111):
+	def __init__(self, x_labelled, x_unlabelled, y_labelled, num_output, hidden_layer_config, corruption_layer_config=[0.2], activation_layer_config=['tanh'], learning_rate_config=[0.02], BATCH_SIZE=400, random_seed=1111):
 		self.hidden_layer_config = hidden_layer_config
 		self.corruption_layer_config = corruption_layer_config
 		self.activation_layer_config = activation_layer_config
@@ -20,26 +25,79 @@ class LadderAE(object):
 		self.x_labelled = x_labelled
 		self.y_labelled = y_labelled
 		self.x_unlabelled = x_unlabelled
+
+		# dimension of the last softmax output layer ..
+
+		self.num_output = num_output
 		self.numpy_rng = np.random.RandomState(random_seed)
 		self.theano_rng = RandomStreams(random_seed)
+		self.denoising_cost = []
+		self.encoders = []
+		self.encoders
+		# self.rstream = 
 
 
 		if len(self.activation_layer_config) == 1:
 			self.activation_layer_config = self.activation_layer_config * self.num_layers 
 
+		if not isinstance(corruption_layer_config, ['list', 'tuple']):
+			raise TypeError()
+
 		if len(self.corruption_layer_config) == 1:
 			self.corruption_layer_config = self.corruption_layer_config * self.num_layers
 
-
+		# last layer will always have a sigmoid non-linearity 
 		self.activation_layer_config += ['sigmoid']
 		self.acts = [None] * self.num_layers
+		self.num_labelled = self.x_labelled.shape[0]
+		self.num_unlabelled = self.x_unlabelled.shape[0]
+		self.num_samples = self.num_labelled + self.num_unlabelled
+		self.num_dims = x_labelled.shape[1]
+		self.x_joined = T.concatenate([x_unlabelled, x_unlabelled], axis=0)
+		self.join = lambda x,y: T.concatenate([x,y], axis=0)
+		self.labelled = lambda x: x[:self.num_labelled,:]
+		self.unlabelled = lambda x: x[self.num_labelled:,:]
+		self.seperate = lambda x: [self.labelled(x), self.unlabelled(x)]
 
 
-	def encoder_pass(self):
+	#  apply enocder fist and then decoder functionality here ...
+	def apply(self):
+		self.encoder_pass()
+		self.decoder_pass()
+		
+
+
+	def encoder_pass(self, inputs, weight):
+		# for i in xrange(self.num_layers):
+			# l = HiddenLayer(hidden_layer_config[i], corruption_layer_config[i], activation=activation_layer_config[i])
+		# h = self.add_gaussian_noise(inputs)
+		# inp_dim = self.num_dims
+		# layer_values = {}
+		# layer_values[1] = {}
+		# layer_values[1]['inp'] = h
+		# # calculate preactivation here ....
+		# for i in 1:self.num_layers:
+		# 	layer_values[i]['W'] = init_shared_weight() 
+		encoders = []
+		corrupt_encoders = []
+		input_labelled = self.x_labelled
+		input_unlabelled = self.x_unlabelled
+
+		noise_std = 1.0
+		# clean encoder pass ...
 		for i in xrange(self.num_layers):
-			l = HiddenLayer(hidden_layer_config[i], corruption_layer_config[i], activation=activation_layer_config[i])
-			
-		# pass
+			le = EncoderLadder(input_labelled, input_unlabelled, self.hidden_layer_config[i])
+			le_corrupt = EncoderLadder(input_labelled_corrupt, input_unlabelled_corrupt, self.hidden_layer_config[i], noise_std=noise_std)
+			encoders.append(le)
+			corrupt_encoders.append(le_corrupt)
+			input_labelled = self.labelled(le.h)
+			input_unlabelled = self.unlabelled(le.h)
+			input_labelled_corrupt = self.labelled(le_corrupt.h)
+			input_unlabelled_corrupt = self.unlabelled(le_corrupt.h)
+
+
+
+	def decoder_pass()
 
 
 	def generate_noise(self, x):
@@ -47,10 +105,10 @@ class LadderAE(object):
 		noise = T.cast(noise_np, theano.config.floatX)
 		return noise
 
-	def add_gaussian_noise(self, x):
+	def add_gaussian_noise(self, x, sigma=1.0):
 		noise = np.random.normal(size=x.shape, loc=0.0, scale=1.0)
-		noise = T.cast(noise, dtype=theano.config.floatX)
-		return x + noise
+		noise = T.cast(noise, dtype=x.dtype)
+		return x + sigma*noise
 
 
 	def normalise_numpy(self, x):
@@ -65,7 +123,6 @@ class LadderAE(object):
 	def initialise_weights(self, n_input, n_output):
 		W =  np.random.randn(size=(n_input, n_output), dtype=theano.config.floatX) / np.sqrt(n_input)
 		return W 
-
 
 
 	def decoder_pass(self):
---
+++ bayesiandnn/neuralnetwork/layers/EncoderLadder.py
@@ -0,0 +1,103 @@
+import  numpy as np
+import theano
+import theano.tensor as T 
+from collections import OrderedDict
+from theano.tensor.shared_randomstreams import RandomStreams 
+
+
+class EncoderLadder(object):
+
+	def __init__(self, x_labelled, x_unlabelled, n_outputs, rstream=None, activation='tanh', noise_std=0.0):
+		
+		self.n_outputs = n_outputs
+		self.rstream = rstream
+		self.layer_values = {}
+		self.n_inputs = x_labelled.shape[0]
+		self.n_outputs = n_outputs 
+		self.num_labels = x_labelled.shape[1]
+		self.num_unlabels = x_unlabelled.shape[1]
+		self.samples = self.num_labels + self.num_unlabels
+		self.W = theano.shared(
+            value=np.asarray(
+                rng.uniform(
+                    low=-6*np.sqrt(6. / (n_inputs + n_outputs)),
+                    high=6*np.sqrt(6. / (n_inputs + n_outputs)),
+                    size=(n_inputs, n_outputs)
+                ),
+                dtype=theano.config.floatX),
+            name='W',
+            borrow=True
+        )
+
+        self.layer_values['W'] = self.W
+        self.join = lambda x,y: T.concatenate([x, y], axis=0)
+        self.x_l = lambda x:x[:N,:]
+        self.x_ul = lambda x:x[N:,:]
+        self.split = lambda x: (self.x_l(x), self.x_ul(x))
+        self.rstream = RandomStreams(seed=11111)
+
+        z_pre = T.dot(self.input, self.W) 
+        if self.corruption == True:
+        	z_pre = utils.add_gaussian_noise(z_pre)
+
+        self.z_pre = z_pre
+        # concatenating the input to one vector
+        self.x_combined = self.join(self.x_labelled, self.x_unlabelled)
+        self.z_l_pre = z_pre[:self.num_labels,:]
+        self.z_ul_pre = z_pre[self.num_labels:,:]
+
+        #  one more hyper-parameter which controls the amount of nois eto be added to the original input.
+        self.noise_std = noise_std
+
+        self.mu_l, self.sigma_l =  self.calculate_moments(self.z_l)
+        self.mu_ul, self.sigma_ul = self.calculate_moments(self.z_pre_ul)
+		z_l = self.batch_normalize(self.z_l)
+		z_ul = self.batch_normalize(self.z_ul)
+		z = self.join(z_l, z_ul)
+
+		# @TODO: introduce momentum to reduce the speed of updating of variables, very important ...
+
+		if self.noise_std:
+			z = z + gen_gaussian_noise(z)*self.noise_std
+
+		self.z = z
+		# final layer activation can only be softmax function ... 
+		if self.layer_type == 'final':
+			activation = 'softmax'
+			h = T.nnet.Softmax(self.gamma* (self.z + self.beta))
+		else:
+			activation = activation
+			activation_fn = "T.nnet." + "relu"
+			h = activation_fn(self.z)
+		self.h = h
+		
+
+
+
+
+    def _calculate_moments(self, x):
+    	mu = T.mean(x, axis=1)
+    	sigma = T.var(x, axis=1)
+    	return mu, sigma
+
+
+    def batch_normalize(self, z, mean=None, var=None):
+    	mu = mean
+    	sigma = var
+    	if not mu or not sigma:
+    		mu, sigma = self._calculate_moments(z)
+    	mu_mat = mu.dimshuffle(0,'x')
+    	sigma_mat = sigma.dimshuffle(0, 'x')
+    	z_norm = (z - mu_mat) / (T.sqrt(sigma_mat + 1e-10))
+    	return z_norm
+
+
+# @TODO: move this to general utils folder, ok for the first draft now .....
+    def gen_gaussian_noise(self, x):
+    	noise = self.rstream.normal(size=x.shape, avg=0.0, std=1.0)
+    	return T.cast(noise, dtype=x.dtype)
+
+
+    def update_batch_normalize(self, x):
+
+
